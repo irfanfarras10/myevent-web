5a62cb8df5d2f1f472d65a9607cd9ee619ad6d0c
Irfan Farras <irfanfarras10@gmail.com>
2022-07-07 23:49:08 +0700

    wip: create register user view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,22 +8,24 @@ use Illuminate\Support\Facades\Http;
 
 class UserController extends Controller
 {
-    public function showEventDetail($id){
+    public function showEventDetail($id)
+    {
         $eventDetailUrl = 'https://myevent-android-api.herokuapp.com/web/events/' . $id;
         $responseData = Http::get($eventDetailUrl);
         $responseLocation = $responseData["venue"];
-        if($responseData["eventVenueCategory"]["id"] == 1){
+        if ($responseData["eventVenueCategory"]["id"] == 1) {
             $location = explode("|", $responseData["venue"]);
             $eventDetailLocationUrl = "https://api.geoapify.com/v1/geocode/reverse?lat=" . $location[0] . "&lon=" . $location[1] . "&apiKey=26018a31a0aa41699818b7b50ea82935";
             $response = Http::get($eventDetailLocationUrl);
             $responseLocation = $response["features"][0]["properties"]["formatted"];
         }
-      
+
         return View::make('event_detail_view')->with('data', $responseData)->with('location', $responseLocation);
     }
-    public function showRegister($id){
-        // var_dump($id);
-        // // $response = Http::get('https://myevent-android-api.herokuapp.com/api/events/3');
-        // // return View::make('register_view');
+    public function showRegister($id)
+    {
+        $eventDetailUrl = 'https://myevent-android-api.herokuapp.com/web/events/' . $id;
+        $responseData = Http::get($eventDetailUrl);
+        return View::make('register_view')->with('data', $responseData);
     }
 }
--- resources/views/register_view.blade.php
+++ resources/views/register_view.blade.php
@@ -1,25 +1,60 @@
+<!DOCTYPE html>
+<html>
 
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<head>
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+	<!--Import materialize.css-->
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet" />
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<style>
+	* {
+		font-family: "Inter", sans-serif;
+	}
 
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	.event-category {
+		background-color: #3b3838;
+		color: white;
+		padding-left: 20px;
+		padding-top: 10px;
+		padding-right: 20px;
+		padding-bottom: 10px;
+		display: inline-block;
+		border-radius: 5px;
+	}
+</style>
+<title>Registrasi Peserta</title>
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
-    <title>Registrasi Peserta</title>
-    
-    <body>
-        <a class="waves-effect waves-light btn">button</a>
-      <!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
-        
+<body>
+	<div class="container">
+		<div class="card">
+			<div class="card-image waves-effect waves-block waves-light">
+				<img class="activator" src={{ $data["bannerPhoto"] }} />
+			</div>
+			<div class="card-content">
+				<div class="row">
+					<div class="col m8 s12">
+						<span class="card-title activator grey-text text-darken-4">
+							{{ $data["name"] }}
+						</span>
+						<p class="event-category">
+							{{ $data["eventCategory"]["name"] }}
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--JavaScript at end of body for optimized loading-->
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+
+</html>
